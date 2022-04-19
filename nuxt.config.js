1264c782f978f7ad238ab198bf58b5f4b393ec13
Kaleb Lape <kaleb.lape@gmail.com>
2022-04-19 10:48:47 -0500

    Tweak config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  app: {
+    cdnURL: 'https://pingortle.github.io/vue-with-shared-components-package/'
+  }
 })
